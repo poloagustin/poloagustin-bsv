8d7b10fbc99a103bd970ed51778e34ac2c433a72
rfernandes <rodrigo.fernandes@accendoit.com>
2015-07-13 18:18:10 -0300

    test

---
+++ SUPERVIELLE/Supervielle/Supervielle.Helpers/Logger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Supervielle.Helpers
+{
+    public static class Logger
+    {
+        private static void Info()
+        {
+            
+        }
+
+        public static void Error(Exception ex)
+        {
+
+        }
+    }
+}
