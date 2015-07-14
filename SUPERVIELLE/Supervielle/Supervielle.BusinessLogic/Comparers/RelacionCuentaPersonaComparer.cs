f6b2bd64c447a93cf33e67b252ec356ba214da4a
Agustin Polo <poloagustin@gmail.com>
2015-07-14 18:54:30 -0300

    added comparers

---
+++ SUPERVIELLE/Supervielle/Supervielle.BusinessLogic/Comparers/RelacionCuentaPersonaComparer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supervielle.BusinessLogic.Comparers
+{
+public static     class RelacionCuentaPersonaComparer
+    {
+        internal static bool CompareCrm(Domain.Crm.bsv_relacion_fisica_cuentas x)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SUPERVIELLE/Supervielle/Supervielle.BusinessLogic/Mappers/RelacionCuentaPersonaMapper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Supervielle.Domain.Crm;
+using Supervielle.Domain.Sql;
+
+namespace Supervielle.BusinessLogic.Mappers
+{
+    public static class RelacionCuentaPersonaMapper
+    {
+        internal static void MapRelacionCuentaPersona(RelacionCuentaPersona sqlRelacionCuentaPersona)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static bsv_relacion_fisica_cuentas GetRelacionCuentaPersona(RelacionCuentaPersona sqlRelacionCuentaPersona)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
